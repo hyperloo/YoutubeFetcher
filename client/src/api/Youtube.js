2a307d618ff75cb4708feb9637590a17bfdfa14e
hyperloo <hack.himanshu1024@gmail.com>
2020-04-11 22:45:05 +0530

    final Commit to add to github

--- client/src/api/Youtube.js
+++ client/src/api/Youtube.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBO2Ye9MqZC9enw_tZ1VkPYjAuz1dtoIMw";
+const KEY = "AIzaSyAZa2kNQc1PAuiYdNfDz0UaJ6GQSxwK4EM";
+
+//AIzaSyBO2Ye9MqZC9enw_tZ1VkPYjAuz1dtoIMw
 
 export const baseParams = {
   part: "snippet",
   maxResults: 15,
-  key: KEY
+  key: KEY,
 };
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3"
+  baseURL: "https://www.googleapis.com/youtube/v3",
 });
